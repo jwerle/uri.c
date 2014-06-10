25f0325e70c2a7559063b53fa8d88f0b5a3adc0f
Joseph Werle <joseph.werle@gmail.com>
2014-06-09 23:46:08 -0400

    another test case

--- test.c
+++ test.c
@@ -34,6 +34,10 @@ main (void) {
     t(uri_encode,
         "`console.log(value); if (!foo) { _bar(): }`",
         "%60console.log(value)%3B%20if%20(!foo)%20%7B%20_bar()%3A%20%7D%60");
+
+    t(uri_encode,
+        "\"it will all be alright !\", he said.",
+        "%22it%20will%20all%20be%20alright%20!%22%2C%20he%20said.");
   }
 
   // decode
